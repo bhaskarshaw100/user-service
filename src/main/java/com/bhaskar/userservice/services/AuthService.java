823fdbbf92d1f2b8ba5c3664b74f2885ea3cd21d
Bhaskar Shaw <bhaskar@Bhaskars-MacBook-Air.local>
2026-09-01 01:19:47 +0530

    Implemented Jwt Token generation

--- src/main/java/com/bhaskar/userservice/services/AuthService.java
+++ src/main/java/com/bhaskar/userservice/services/AuthService.java
@@ -1,15 +1,15 @@
 package com.bhaskar.userservice.services;
 
-import com.bhaskar.userservice.exceptions.UserAlreadyExistException;
 import com.bhaskar.userservice.exceptions.UserNotFoundException;
 import com.bhaskar.userservice.exceptions.WrongPasswordException;
 import com.bhaskar.userservice.models.User;
 import com.bhaskar.userservice.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -51,9 +51,25 @@ public class AuthService {
                 .matches(password, userOptional.get().getPassword());
 
         if (matches) {
-            return "token";
+            return createJwtToken(userOptional.get().getId(),
+                    new ArrayList<>(),
+                    userOptional.get().getEmail());
         } else {
             throw new WrongPasswordException("Incorrect password for user with email " + email);
         }
+    }
+
+    private String createJwtToken(Long userId, List<String> roles, String email) {
+        Map<String, Object> dataInJwt = new HashMap<>();
+        dataInJwt.put("userId", userId);
+        dataInJwt.put("roles", roles);
+        dataInJwt.put("email", email);
+
+        return Jwts.builder()
+                .claims(dataInJwt)
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .signWith(Jwts.SIG.HS256.key().build())
+                .compact();
     }
 }
